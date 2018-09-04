e5243d0f3d113c68319e4182605da4cc5ec6772e
Wilco <wilco.hansen@outlook.com>
2018-09-04 13:11:41 +0200

    afgerond
    De opdracht is afgerond door gebruik te maken van  var

--- Lab 2/Lab2.js
+++ Lab 2/Lab2.js
@@ -2,10 +2,17 @@
  Applicatie en mediaontwikkelaar*/
 
 
-alert("Welkom op de Lab1 website, zou u zo vrij wilen zijn 3 vragen te beantwoorden."); // geeft een melding op de website wanneer je de webpagina laad, de alert verteld iets.
+const bericht= 'Welkom op de Lab2 website, zou u zo vrij wilen zijn 3 vragen te beantwoorden.'; // zorgt er voor dat bericht wordt gelinked met de tekst.
+alert(bericht);
 
-document.write( 'Naam:  ' + prompt('Wat is uw voornaam?') + '<br>' ); // geeft wat er word ingevoerd weer op de website
+var Voornaam = prompt('Wat is uw voornaam?');
 
-document.write( 'Achternaam:  ' + prompt('Wat is uw achternaam?') + '<br>'); // geeft wat er word ingevoerd weer op de website
+var Achternaam = prompt('Wat is uw Achternaam?');
 
-document.write( 'Leeftijd:  ' + prompt('Wat is uw leeftijd?') + '<br>'); // geeft wat er word ingevoerd weer op de website
+var Leeftijd = prompt('Wat is uw leeftijd?');
+
+document.write( 'Naam:  ' + Voornaam + '<br>' ); // geeft wat er word ingevoerd weer op de website
+
+document.write( 'Achternaam:  ' + Achternaam + '<br>'); // geeft wat er word ingevoerd weer op de website
+
+document.write( 'Leeftijd:  ' + Leeftijd + '<br>'); // geeft wat er word ingevoerd weer op de website
