fe9e325035aad59a46369534f96cac930b76fb31
Wilco <wilco.hansen@outlook.com>
2018-09-04 11:49:10 +0200

    Created Lab 2 files and copied Lab1 and paste it in Lab2

---
+++ Lab 2/Lab2.js
@@ -0,0 +1,11 @@
+/* Wilco Hansen
+ Applicatie en mediaontwikkelaar*/
+
+
+alert("Welkom op de Lab1 website, zou u zo vrij wilen zijn 3 vragen te beantwoorden."); // geeft een melding op de website wanneer je de webpagina laad, de alert verteld iets.
+
+document.write( 'Naam:  ' + prompt('Wat is uw voornaam?') + '<br>' ); // geeft wat er word ingevoerd weer op de website
+
+document.write( 'Achternaam:  ' + prompt('Wat is uw achternaam?') + '<br>'); // geeft wat er word ingevoerd weer op de website
+
+document.write( 'Leeftijd:  ' + prompt('Wat is uw leeftijd?') + '<br>'); // geeft wat er word ingevoerd weer op de website
