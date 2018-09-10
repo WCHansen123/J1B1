e22df7c87507146cf14b9130c1663cf4381f7eaa
Wilco <wilco.hansen@outlook.com>
2018-09-10 11:17:57 +0200

    final working version

--- Pizza calculator/app.js
+++ Pizza calculator/app.js
@@ -3,6 +3,15 @@
 var Small = prompt("hoeveel kleine pizza's wilt u");
 var Medium = prompt("hoeveel middel pizza's wilt u");
 var Large = prompt("hoeveel grote pizza's wilt u");
-document.write( 'small pizza prijs: € ' +4.30 * Small + "<br>");
-document.write( 'medium pizza prijs: € ' +7.20 * Medium + "<br>");
-document.write( 'Large pizza prijs: € ' + 11.23 * Large + "<br>");
+
+const Psmall = Small  * 5.00;
+const Pmedium = Medium  * 7.20;
+const Plarge = Large * 11.25;
+
+document.write('small pizza prijs: € ' + Psmall  + "<br>");
+document.write('medium pizza prijs: € ' + Pmedium  + "<br>");
+document.write('Large pizza prijs: € ' +  Plarge  + "<br>" + "<br>");
+
+
+var Totaal = Psmall + Pmedium + Plarge;
+document.write('Totaal:' + Totaal);
