842e485d8b886ff998e8ab55e4c79149d5e91be8
Wilco <wilco.hansen@outlook.com>
2018-09-10 12:19:27 +0200

    extra safe

--- Pizza calculator/app.js
+++ Pizza calculator/app.js
@@ -4,14 +4,14 @@ var Small = prompt("hoeveel kleine pizza's wilt u");
 var Medium = prompt("hoeveel middel pizza's wilt u");
 var Large = prompt("hoeveel grote pizza's wilt u");
 
-const Psmall = Small  * 5.00;
-const Pmedium = Medium  * 7.20;
-const Plarge = Large * 11.25;
+const Prijssmall = Small  * 5.00;
+const Prijsmedium = Medium  * 7.20;
+const Prijslarge = Large * 11.25;
 
-document.write('small pizza prijs: € ' + Psmall  + "<br>");
-document.write('medium pizza prijs: € ' + Pmedium  + "<br>");
-document.write('Large pizza prijs: € ' +  Plarge  + "<br>" + "<br>");
+document.write('small pizza prijs: € ' + Prijssmall  + "<br>");
+document.write('medium pizza prijs: € ' + Prijsmedium  + "<br>");
+document.write('Large pizza prijs: € ' +  Prijslarge  + "<br>" + "<br>");
 
 
-var Totaal = Psmall + Pmedium + Plarge;
+var Totaal = Prijssmall + Prijsmedium + Prijslarge;
 document.write('Totaal:' + Totaal);
