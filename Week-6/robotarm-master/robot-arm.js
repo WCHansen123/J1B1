cb882c201835b7094db862a9d1aed20882db2db3
Wilco <wilco.hansen@outlook.com>
2018-10-08 17:52:42 +0200

    -

--- Week-6/robotarm-master/robot-arm.js
+++ Week-6/robotarm-master/robot-arm.js
@@ -16,7 +16,7 @@ var RobotArm = function (canvas) {
     // The amount of rows to use
     self.rows = 8;
     // The speed of which the animations go
-    self.speed = 300;
+    self.speed = 700;
 
     // List of animations
     local.animationList = [];
